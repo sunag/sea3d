caa1fb72842c5ae06fad410c73c7ba98a7714ef7
sunag <contact@sunag.com.br>
2016-11-20 02:18:24 -0200

    cleanup

--- Player/src/SEA3DNodeMaterial.js
+++ Player/src/SEA3DNodeMaterial.js
@@ -11,17 +11,17 @@
 
 THREE.SEA3D.MaterialBuilder = {
 
-	apply : function( scope ) {
+	apply: function ( scope ) {
 
 		Object.defineProperties( this, {
 			enabledRim: {
 				enumerable: true,
-				get: function() {
+				get: function () {
 
 					return this.rimColor != null;
 
 				},
-				set: function( value ) {
+				set: function ( value ) {
 
 					if ( this.enabledRim == !! value ) return;
 
@@ -71,39 +71,39 @@ THREE.SEA3D.MaterialBuilder = {
 			},
 			enabledWrapLighting: {
 				enumerable: true,
-				get: function() {
+				get: function () {
 
 					return this.wrapColor != null;
 
 				},
-				set: function( val ) {
+				set: function ( val ) {
 
 					if ( this.enabledWrapLighting == !! val ) return;
 
-					if (val) {
-						
+					if ( val ) {
+
 						this.wrapColor = new THREE.ColorNode( 0x6b0602 );
 						this.wrapLight = new THREE.FloatNode( 1.5 );
 						this.wrapShadow = new THREE.FloatNode( 0 );
-						
+
 					} else {
-						
+
 						delete this.wrapColor;
 						delete this.wrapLight;
 						delete this.wrapShadow;
-						
+
 					}
 
 				}
 			},
 			rimIntensity: {
 				enumerable: true,
-				get: function() {
+				get: function () {
 
 					return this.rimIntensityValue;
 
 				},
-				set: function( val ) {
+				set: function ( val ) {
 
 					this.rimIntensityValue = val;
 
@@ -114,12 +114,12 @@ THREE.SEA3D.MaterialBuilder = {
 			},
 			enabledFresnel: {
 				enumerable: true,
-				get: function() {
+				get: function () {
 
 					return this.fresnel != null;
 
 				},
-				set: function( value ) {
+				set: function ( value ) {
 
 					if ( this.enabledFresnel == !! value ) return;
 
@@ -152,8 +152,7 @@ THREE.SEA3D.MaterialBuilder = {
 							THREE.OperatorNode.MUL
 						);
 
-					}
-					else {
+					}					else {
 
 						delete this.fresnelReflectance;
 						delete this.fresnelPower;
@@ -172,33 +171,33 @@ THREE.SEA3D.MaterialBuilder = {
 
 	},
 
-	build : function() {
+	build: function () {
 
 		// DIFFUSE
 
 		var diffuse = this.diffuse;
-		
+
 		// COLOR_REPLACE
-		
-		if (diffuse && this.enabledColorReplace) {
-		
+
+		if ( diffuse && this.enabledColorReplace ) {
+
 			var red = new THREE.OperatorNode( new THREE.SwitchNode( diffuse, 'r' ), this.red, THREE.OperatorNode.MUL );
 			var green = new THREE.OperatorNode( new THREE.SwitchNode( diffuse, 'g' ), this.green, THREE.OperatorNode.MUL );
 			var blue = new THREE.OperatorNode( new THREE.SwitchNode( diffuse, 'b' ), this.blue, THREE.OperatorNode.MUL );
-			
-			var colors = new THREE.OperatorNode( red, new THREE.OperatorNode(green, blue, THREE.OperatorNode.ADD), THREE.OperatorNode.ADD );
-			
-			diffuse = new THREE.JoinNode( 
+
+			var colors = new THREE.OperatorNode( red, new THREE.OperatorNode( green, blue, THREE.OperatorNode.ADD ), THREE.OperatorNode.ADD );
+
+			diffuse = new THREE.JoinNode(
 				new THREE.SwitchNode( colors, 'r' ),
 				new THREE.SwitchNode( colors, 'g' ),
 				new THREE.SwitchNode( colors, 'b' ),
 				new THREE.SwitchNode( diffuse, 'a' )
 			);
-		
+
 		}
-		
+
 		// DIFFUSE_OPACITY
-		
+
 		var alpha = this.opacity < 1 ? new THREE.FloatNode( this.opacity ) : undefined;
 
 		if ( diffuse && this.diffuseTransparent ) {
@@ -209,9 +208,9 @@ THREE.SEA3D.MaterialBuilder = {
 			else alpha = diffuseAlpha;
 
 		}
-		
+
 		// ALPHA
-		
+
 		if ( this.alphaDiffuse ) {
 
 			var alphaDiffuse = new THREE.SwitchNode( this.alphaDiffuse, 'r' );
@@ -228,25 +227,25 @@ THREE.SEA3D.MaterialBuilder = {
 		if ( detailDiffuse ) {
 
 			detailDiffuse.coord = new THREE.OperatorNode( new THREE.UVNode(), this.detailScale, THREE.OperatorNode.MUL );
-		
-			if (diffuse) {
-			
-				switch (this.detailBlendMode) {
-				
+
+			if ( diffuse ) {
+
+				switch ( this.detailBlendMode ) {
+
 					case "multiply":
 						diffuse = new THREE.OperatorNode( diffuse, detailDiffuse, THREE.OperatorNode.MUL );
 						break;
-				
+
 				}
-				
+
 			} else {
-			
+
 				diffuse = detailDiffuse;
-			
+
 			}
 
 		}
-		
+
 		this.color = diffuse || new THREE.ColorNode( 0xEEEEEE );
 
 		if ( alpha ) this.alpha = alpha;
@@ -277,18 +276,18 @@ THREE.SEA3D.MaterialBuilder = {
 			else shadow = lightDiffuse;
 
 		}
-		
+
 		// WRAP LIGHT
-		
+
 		var wrapColor = new THREE.ColorNode( 0x6b0602 );
-					var wrapLight = new THREE.FloatNode( 1.5 );
-					var wrapShadow = new THREE.FloatNode( 0 );
-		
-		if (this.wrapColor) {
-			
+		var wrapLight = new THREE.FloatNode( 1.5 );
+		var wrapShadow = new THREE.FloatNode( 0 );
+
+		if ( this.wrapColor ) {
+
 			var wrapDirectLight = new THREE.LightNode();
 			var wrapLightLuminance = new THREE.LuminanceNode( wrapDirectLight );
-			
+
 			var lightWrap = new THREE.Math3Node(
 				this.wrapShadow,
 				this.wrapLight,
@@ -300,35 +299,35 @@ THREE.SEA3D.MaterialBuilder = {
 				new THREE.ConstNode( THREE.ConstNode.PI2 ),
 				THREE.OperatorNode.MUL
 			);
-			
+
 			var wrappedLight = new THREE.Math1Node( lightTransition, THREE.Math1Node.SIN );
-			
+
 			var wrappedLightColor = new THREE.OperatorNode(
 				wrappedLight,
 				this.wrapColor,
 				THREE.OperatorNode.MUL
 			);
-			
+
 			var wrappedArea = new THREE.Math1Node( wrappedLightColor, THREE.Math1Node.SAT );
-			
+
 			var wrappedTotalLight = new THREE.OperatorNode(
 				wrapDirectLight,
 				wrappedArea,
 				THREE.OperatorNode.ADD
 			);
-			
-			if (light) light = new THREE.OperatorNode( light, wrappedTotalLight, THREE.OperatorNode.ADD );
+
+			if ( light ) light = new THREE.OperatorNode( light, wrappedTotalLight, THREE.OperatorNode.ADD );
 			else light = wrappedTotalLight;
-			
+
 		}
-		
+
 		this.light = light;
 		this.shadow = shadow;
-		
+
 		// AMBIENT
 
 		var ambient;
-		
+
 		if ( this.ambientColor ) {
 
 			ambient = new THREE.OperatorNode(
@@ -336,31 +335,31 @@ THREE.SEA3D.MaterialBuilder = {
 				this.ambientColor,
 				THREE.OperatorNode.MUL
 			);
-			
+
 		}
-		
+
 		this.ambient = ambient;
 
 		// ENVIRONMENT ( PHONG )
 
 		var environment, environmentAlpha;
 
-		//-- ADD REFLECT 
-		
-		if (this.reflection) {
+		//-- ADD REFLECT
+
+		if ( this.reflection ) {
 
 			environment = this.reflection;
 			environmentAlpha = this.reflectionAlpha;
 
 		}
-		
+
 		if ( this.enabledRim ) {
 
-			if (this.emissive) this.emissive = new THREE.OperatorNode(this.emissive, this.rim, THREE.OperatorNode.ADD);
+			if ( this.emissive ) this.emissive = new THREE.OperatorNode( this.emissive, this.rim, THREE.OperatorNode.ADD );
 			else this.emissive = this.rim;
 
-		} else if (this.enabledFresnel) {
-		
+		} else if ( this.enabledFresnel ) {
+
 			var fresnel = new THREE.Math1Node( this.fresnel, THREE.Math1Node.SAT );
 
 			if ( environmentAlpha ) {
@@ -374,12 +373,12 @@ THREE.SEA3D.MaterialBuilder = {
 			}
 
 			environmentAlpha = fresnel;
-		
+
 		}
-		
+
 		this.environment = environment;
 		this.environmentAlpha = environmentAlpha;
-		
+
 		// COMPLETE
 
 		THREE.NodeMaterial.prototype.build.call( this );
@@ -391,7 +390,7 @@ THREE.SEA3D.MaterialBuilder = {
 //	Phong Material
 //
 
-THREE.SEA3D.PhongMaterial = function() {
+THREE.SEA3D.PhongMaterial = function () {
 
 	THREE.PhongNodeMaterial.call( this );
 
@@ -401,7 +400,7 @@ THREE.SEA3D.PhongMaterial = function() {
 
 THREE.SEA3D.PhongMaterial.prototype = Object.assign( Object.create( THREE.PhongNodeMaterial.prototype ), {
 
-	constructor : THREE.SEA3D.PhongMaterial
+	constructor: THREE.SEA3D.PhongMaterial
 
 } );
 
@@ -409,7 +408,7 @@ THREE.SEA3D.PhongMaterial.prototype = Object.assign( Object.create( THREE.PhongN
 //	Standard Material
 //
 
-THREE.SEA3D.StandardMaterial = function() {
+THREE.SEA3D.StandardMaterial = function () {
 
 	THREE.StandardNodeMaterial.call( this );
 
@@ -419,7 +418,7 @@ THREE.SEA3D.StandardMaterial = function() {
 
 THREE.SEA3D.StandardMaterial.prototype = Object.assign( Object.create( THREE.StandardNodeMaterial.prototype ), {
 
-	constructor : THREE.SEA3D.StandardMaterial
+	constructor: THREE.SEA3D.StandardMaterial
 
 } );
 
@@ -427,8 +426,8 @@ THREE.SEA3D.StandardMaterial.prototype = Object.assign( Object.create( THREE.Sta
 //	Standard Material
 //
 
-THREE.SEA3D.prototype.applyEnvironment = function(envMap) {
-	
+THREE.SEA3D.prototype.applyEnvironment = function ( envMap ) {
+
 	for ( var j = 0, l = this.materials.length; j < l; ++ j ) {
 
 		var mat = this.materials[ j ];
@@ -444,13 +443,13 @@ THREE.SEA3D.prototype.applyEnvironment = function(envMap) {
 		}
 
 	}
-	
+
 };
 
 
-THREE.SEA3D.prototype.createMaterial = function( sea ) {
+THREE.SEA3D.prototype.createMaterial = function ( sea ) {
 
-	if (sea.tecniquesDict[ SEA3D.Material.PHYSICAL ]) {
+	if ( sea.tecniquesDict[ SEA3D.Material.PHYSICAL ] ) {
 
 		return new THREE.SEA3D.StandardMaterial();
 
@@ -463,12 +462,12 @@ THREE.SEA3D.prototype.createMaterial = function( sea ) {
 THREE.SEA3D.prototype.defaultMaterialTechnique = THREE.SEA3D.prototype.materialTechnique;
 
 THREE.SEA3D.prototype.materialTechnique =
-( function() {
+( function () {
 
-	var techniques = {}
+	var techniques = {};
 
 	// FINAL
-	techniques.onComplete = function( mat, sea ) {
+	techniques.onComplete = function ( mat, sea ) {
 
 		this.defaultMaterialTechnique.onComplete( mat, sea );
 
@@ -478,7 +477,7 @@ THREE.SEA3D.prototype.materialTechnique =
 
 	// PHYSICAL
 	techniques[ SEA3D.Material.PHYSICAL ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.diffuse = new THREE.ColorNode( tech.color );
 		mat.roughness.number = tech.roughness;
@@ -488,7 +487,7 @@ THREE.SEA3D.prototype.materialTechnique =
 
 	// REFLECTIVITY
 	techniques[ SEA3D.Material.REFLECTIVITY ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.reflectivity = new THREE.FloatNode( tech.strength );
 
@@ -496,7 +495,7 @@ THREE.SEA3D.prototype.materialTechnique =
 
 	// CLEAR_COAT
 	techniques[ SEA3D.Material.CLEAR_COAT ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.clearCoat = new THREE.FloatNode( tech.strength );
 		mat.clearCoatRoughness = new THREE.FloatNode( tech.roughness );
@@ -505,7 +504,7 @@ THREE.SEA3D.prototype.materialTechnique =
 
 	// PHONG
 	techniques[ SEA3D.Material.PHONG ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.ambientColor = new THREE.ColorNode( tech.ambientColor );
 		mat.diffuse = new THREE.ColorNode( tech.diffuseColor );
@@ -517,7 +516,7 @@ THREE.SEA3D.prototype.materialTechnique =
 
 	// DIFFUSE_MAP
 	techniques[ SEA3D.Material.DIFFUSE_MAP ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.diffuse = new THREE.TextureNode( tech.texture.tag );
 		mat.diffuseTransparent = tech.texture.transparent;
@@ -526,7 +525,7 @@ THREE.SEA3D.prototype.materialTechnique =
 
 	// ROUGHNESS_MAP
 	techniques[ SEA3D.Material.ROUGHNESS_MAP ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.roughness = new THREE.TextureNode( tech.texture.tag );
 
@@ -534,7 +533,7 @@ THREE.SEA3D.prototype.materialTechnique =
 
 	// METALNESS_MAP
 	techniques[ SEA3D.Material.METALNESS_MAP ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.metalness = new THREE.TextureNode( tech.texture.tag );
 
@@ -542,7 +541,7 @@ THREE.SEA3D.prototype.materialTechnique =
 
 	// SPECULAR_MAP
 	techniques[ SEA3D.Material.SPECULAR_MAP ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.specularMap = new THREE.TextureNode( tech.texture.tag );
 
@@ -550,7 +549,7 @@ THREE.SEA3D.prototype.materialTechnique =
 
 	// NORMAL_MAP
 	techniques[ SEA3D.Material.NORMAL_MAP ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.normal = new THREE.TextureNode( tech.texture.tag );
 
@@ -559,7 +558,7 @@ THREE.SEA3D.prototype.materialTechnique =
 	// REFLECTION
 	techniques[ SEA3D.Material.REFLECTION ] =
 	techniques[ SEA3D.Material.FRESNEL_REFLECTION ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.reflection = new THREE.CubeTextureNode( tech.texture.tag );
 		mat.reflectionAlpha = new THREE.FloatNode( tech.alpha );
@@ -576,27 +575,27 @@ THREE.SEA3D.prototype.materialTechnique =
 
 	// REFLECTION_SPHERICAL
 	techniques[ SEA3D.Material.REFLECTION_SPHERICAL ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.reflection = new THREE.TextureNode( tech.texture.tag, new THREE.ReflectNode( THREE.ReflectNode.SPHERE ) );
 		mat.reflectionAlpha = new THREE.FloatNode( tech.alpha );
 
 	};
-	
+
 	// COLOR_REPLACE
 	techniques[ SEA3D.Material.COLOR_REPLACE ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.enabledColorReplace = true;
-		mat.red = new THREE.ColorNode( tech.red ); 
-		mat.green = new THREE.ColorNode( tech.green ); 
-		mat.blue = new THREE.ColorNode( tech.blue ); 
+		mat.red = new THREE.ColorNode( tech.red );
+		mat.green = new THREE.ColorNode( tech.green );
+		mat.blue = new THREE.ColorNode( tech.blue );
 
 	};
-	
+
 	// RIM
 	techniques[ SEA3D.Material.RIM ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.enabledRim = true;
 		mat.rimColor.value.setHex( tech.color );
@@ -607,36 +606,36 @@ THREE.SEA3D.prototype.materialTechnique =
 
 	// WRAP_LIGHTING
 	techniques[ SEA3D.Material.WRAP_LIGHTING ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.enabledWrapLighting = true;
 		mat.wrapColor.value.setHex( tech.color );
 		mat.wrapShadow.number = 1.0 - tech.strength;
 
 	};
-	
+
 	// LIGHT_MAP
 	techniques[ SEA3D.Material.LIGHT_MAP ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.lightDiffuse = new THREE.TextureNode( tech.texture.tag, new THREE.UVNode( tech.channel ) );
 		mat.lightBlendMode = tech.blendMode;
 
 	};
-	
+
 	// DETAIL_MAP
 	techniques[ SEA3D.Material.DETAIL_MAP ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.detailDiffuse = new THREE.TextureNode( tech.texture.tag );
 		mat.detailBlendMode = tech.blendMode;
 		mat.detailScale = new THREE.FloatNode( tech.scale );
 
 	};
-	
+
 	// ALPHA_MAP
 	techniques[ SEA3D.Material.ALPHA_MAP ] =
-	function( mat, tech ) {
+	function ( mat, tech ) {
 
 		mat.alphaDiffuse = new THREE.TextureNode( tech.texture.tag );
 
